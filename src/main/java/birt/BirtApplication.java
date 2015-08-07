2fd88ea44aa500b6ced22de88fd71e512ebe31fe
Madalin.Colezea <12345@qwert>
2015-08-07 10:23:14 +0300

    Initial commit

---
+++ src/main/java/birt/BirtApplication.java
@@ -0,0 +1,12 @@
+package birt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirtApplication.class, args);
+    }
+}
